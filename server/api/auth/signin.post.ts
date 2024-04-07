d0b0f0f7929a53dcbddde5b29b7081b9cd5460f4
Yunus <19399214+yunusefendi52@users.noreply.github.com>
2024-04-08 00:03:35 +0700

    removejsonwebtoken first

--- server/api/auth/signin.post.ts
+++ server/api/auth/signin.post.ts
@@ -1,16 +1,19 @@
-import jsonwebtoken from 'jsonwebtoken'
 import { JWT_KEY } from '~/server/utils/utils'
 
 export default defineEventHandler(async (event) => {
-    const token = jsonwebtoken.sign({
-        name: 'Yunus',
-        userId: 1,
-    }, JWT_KEY, {})
-    setCookie(event, 'app-auth', token, {
-        httpOnly: false,
-        secure: true,
-        sameSite: 'lax',
-    })
+    // const token = await new jose.SignJWT({
+    //     name: 'Yunus',
+    //     userId: 'b287aa5d85a040f78aa53a2ff7d53023'
+    // }).sign(JWT_KEY)
+    // // const token = jose.sign({
+    // //     name: 'Yunus',
+    // //     userId: 1,
+    // // }, JWT_KEY, {})
+    // setCookie(event, 'app-auth', token, {
+    //     httpOnly: false,
+    //     secure: true,
+    //     sameSite: 'lax',
+    // })
     return {
         ok: true,
     }
--- server/middleware/auth-middleware.ts
+++ server/middleware/auth-middleware.ts
@@ -1,4 +1,3 @@
-import jsonwebtoken from 'jsonwebtoken'
 import { JWT_KEY } from '../utils/utils';
 
 export type AuthData = {
@@ -13,18 +12,5 @@ declare module 'h3' {
 
 
 export default defineEventHandler((event) => {
-  const appAuth = getCookie(event, 'app-auth')
-  if (appAuth) {
-    const verifiedData = jsonwebtoken.verify(appAuth, JWT_KEY)
-    if (!verifiedData) {
-      deleteCookie(event, 'app-auth')
-    } else {
-      // @ts-ignore
-      const userId = verifiedData.userId
-      event.context.auth = { userId: userId }
-    }
-  } else {
-    console.log('user not logged in')
-    event.context.auth = { userId: '8551be3fcf41470b806a6334d24396d6' }
-  }
+  event.context.auth = { userId: '8551be3fcf41470b806a6334d24396d6' }
 })
