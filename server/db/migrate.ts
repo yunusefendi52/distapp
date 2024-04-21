bb5e6e464a6d2ea5f9868f93a179a39a44fa10b0
Yunus <19399214+yunusefendi52@users.noreply.github.com>
2024-04-21 17:55:34 +0700

    Add some script

--- server/db/migrate.ts
+++ server/db/migrate.ts
@@ -2,7 +2,7 @@ import db from './db';
 import { migrate } from 'drizzle-orm/libsql/migrator';
 
 // This will run migrations on the database, skipping the ones already applied
-await migrate(db(process.env), { migrationsFolder: './drizzle' });
+await migrate(db(process.env), { migrationsFolder: `${import.meta.dirname}/drizzle` });
 
 // // Don't forget to close the connection, otherwise the script will hang
 // await connection.end();
