74975f6e81f461d625dc1ed0ab655d92a532c6d3
Yunus <yunus.rectangle951@silomails.com>
2024-04-23 22:50:45 +0700

    try `s3`

--- server/api/artifacts/upload-artifact.post.ts
+++ server/api/artifacts/upload-artifact.post.ts
@@ -1,6 +1,6 @@
 import { v4 } from "uuid";
 import { getStorageKeys, s3BucketName } from "~/server/utils/utils";
-import { createS3 } from "~/server/services/s3"
+import { createS3O } from "~/server/services/s3"
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 
@@ -8,7 +8,7 @@ export default defineEventHandler(async (event) => {
     const key = v4()
     var expires = 300;
     const { temp } = getStorageKeys(event.context.auth, key)
-    const s3 = createS3(event)
+    const s3 = createS3O(event)
     const signedUrl = await getSignedUrl(s3, new PutObjectCommand({
         Bucket: s3BucketName,
         Key: temp,
--- server/services/s3.ts
+++ server/services/s3.ts
@@ -1,4 +1,5 @@
 import {
+    S3,
     S3Client,
 } from "@aws-sdk/client-s3";
 import type { EventHandlerRequest, H3Event } from "h3";
@@ -7,6 +8,20 @@ export const createS3 = (event: H3Event<EventHandlerRequest>) => {
     const env = event.context.cloudflare?.env ?? process.env
     const { S3_ENDPOINT, S3_ACCESS_KEY_ID, S3_SECRET_ACCESS_KEY } = useRuntimeConfig(event)
     const s3 = new S3Client({
+        endpoint: S3_ENDPOINT,
+        region: 'auto',
+        credentials: {
+            accessKeyId: S3_ACCESS_KEY_ID,
+            secretAccessKey: S3_SECRET_ACCESS_KEY,
+        },
+    })
+    return s3
+}
+
+export const createS3O = (event: H3Event<EventHandlerRequest>) => {
+    const env = event.context.cloudflare?.env ?? process.env
+    const { S3_ENDPOINT, S3_ACCESS_KEY_ID, S3_SECRET_ACCESS_KEY } = useRuntimeConfig(event)
+    const s3 = new S3({
         endpoint: S3_ENDPOINT,
         region: 'auto',
         credentials: {
