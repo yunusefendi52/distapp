acef6c1a520786dcdec276bb55d37cecdbc5c570
Yunus <yunus.rectangle951@silomails.com>
2024-06-22 21:48:04 +0700

    Prevent create app when role is not admin

--- server/api/create-app.post.ts
+++ server/api/create-app.post.ts
@@ -1,9 +1,23 @@
-import { apps } from '../db/schema'
+import { eq } from 'drizzle-orm'
+import { apps, organizations } from '../db/schema'
 import { generateId } from '../utils/utils'
+import { takeUniqueOrThrow } from './detail-app.get'
 
 export default defineEventHandler(async (event) => {
     const request = await readBody(event)
     const db = event.context.drizzle
+    const org = await db.select({
+        orgName: organizations.name,
+    })
+        .from(organizations)
+        .where(eq(organizations.id, request.orgId))
+        .then(takeUniqueOrThrow)
+    if (await roleEditNotAllowed(event, org.orgName)) {
+        throw createError({
+            message: 'Unauthorized',
+            statusCode: 401,
+        })
+    }
     await db.insert(apps).values({
         id: generateId(),
         displayName: request.name,
