47f20527a3c43cba579e3d7350c33afd9bb28d6b
Yunus <yunus.rectangle951@silomails.com>
2024-07-27 11:59:49 +0700

    Fix active menu

--- layouts/AppMenu.vue
+++ layouts/AppMenu.vue
@@ -4,11 +4,13 @@ const orgsStore = await useOrgsStore()
 const items = computed(() => [{
     label: 'All Apps',
     icon: 'pi pi-th-large',
-    route: '/apps/'
+    route: '/apps/',
+    name: undefined,
 }, ...orgsStore.orgs.value.map((e) => ({
     label: e.displayName,
     icon: 'pi pi-users',
-    route: `/orgs/${e.name}/`
+    route: `/orgs/${e.name}/`,
+    name: e.name,
 }))])
 
 const route = useRoute()
@@ -20,12 +22,14 @@ const orgName = computed(() => route.params.orgName as string)
     <div class="p-2 flex gap-2 flex-column">
         <router-link v-for="(item, index) in items" :key="index" :to="item.route" custom
             v-slot="{ href, route, navigate, isActive, isExactActive }">
-            <a :active="item.route.endsWith(orgName) || routePath === route.path" :href="href" @click="navigate" class="cursor-pointer">
+            <a :active="item.route.endsWith(orgName) || item.name === orgName || routePath === route.path" :href="href"
+                @click="navigate" class="cursor-pointer">
                 <div class="flex flex-row px-3 py-2.5 gap-3 rounded-md" style="align-items: center;"
-                    :class="{ 'menu-active': item.route.endsWith(orgName) || routePath === route.path }">
+                    :class="{ 'menu-active': item.route.endsWith(orgName) || item.name === orgName || routePath === route.path }">
                     <i class="text-xl" :class="item.icon">
                     </i>
-                    <label :class="{ 'font-bold': item.route.endsWith(orgName) || routePath === route.path }">{{
+                    <label
+                        :class="{ 'font-bold': item.route.endsWith(orgName) || item.name === orgName || routePath === route.path }">{{
                         item.label }}</label>
                 </div>
             </a>
