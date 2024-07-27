b4cd968826e40e80802a2cbd4abce0b8d9b368c2
Yunus <yunus.rectangle951@silomails.com>
2024-07-27 11:59:49 +0700

    Add params in redirect trailing slash

--- middleware/01.trailing-slash-append.global.ts
+++ middleware/01.trailing-slash-append.global.ts
@@ -1,12 +1,15 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     if (to.path !== '/' && !to.path.endsWith('/')) {
-        const { path, query, hash } = to
+        const { path, query, hash, params } = to
         const nextPath = `${path}/`
         const nextRoute = {
             path: nextPath,
             query,
             hash,
+            params,
         }
-        return navigateTo(nextRoute, { redirectCode: 302 })
+        return navigateTo(nextRoute, {
+            redirectCode: 302,
+        })
     }
 })
