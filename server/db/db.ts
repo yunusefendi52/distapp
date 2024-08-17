492eeb46911031e3867b200e65edd65b271097b6
Yunus <yunus.rectangle951@silomails.com>
2024-08-17 17:22:10 +0700

    Disable limit concurrency drizzle

--- server/db/db.ts
+++ server/db/db.ts
@@ -5,5 +5,6 @@ import * as schema from '../db/schema'
 const db = (env: any, migrationMode: boolean = false) => drizzle(createClient({
     url: env.DB_URL ?? env.NUXT_DB_URL,
     authToken: env.DB_AUTH_TOKEN ?? env.NUXT_DB_AUTH_TOKEN,
+    concurrency: 0,
 }), { schema, logger: migrationMode ? false : env?.enableLogging ?? env?.NUXT_APP_ENABLE_DRIZZLE_LOGGING ?? true });
 export default db
