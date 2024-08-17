e57c6acd21eccd0519f7f80b7d92257f55d4cbcb
Yunus <yunus.rectangle951@silomails.com>
2024-08-17 12:52:52 +0700

    Add catch all install link

---
+++ pages/install/[publicId]/[...catchPublicId].vue
@@ -0,0 +1,15 @@
+<template>
+    Redirecting
+</template>
+
+<script setup lang="ts">
+const { params } = useRoute()
+
+navigateTo({
+    name: 'install-publicId',
+    params: {
+        publicId: params.publicId,
+    },
+    redirectCode: 302,
+})
+</script>
