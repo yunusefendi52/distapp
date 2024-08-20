d314841bdc1742cb72791d843fe1a7d472548a6b
Yunus <yunus.rectangle951@silomails.com>
2024-08-20 20:08:15 +0700

    Fix app bar height incosistency

--- components/AppBarContainer.vue
+++ components/AppBarContainer.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="flex flex-col gap-3 sm:flex-row px-6 items-center py-3">
+        <div class="flex flex-col gap-3 sm:flex-row px-6 items-center py-3" style="min-height: 60px;">
             <slot></slot>
         </div>
         <AppDivider orientation="horizontal" />
