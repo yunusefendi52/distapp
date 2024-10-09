434cbbd4d955970a16f138664bff5991cc60a632
Yunus <yunus.rectangle951@silomails.com>
2024-10-09 23:12:57 +0700

    Add delete org

--- pages/orgs/[orgName]/settings/danger.vue
+++ pages/orgs/[orgName]/settings/danger.vue
@@ -1,3 +1,13 @@
 <template>
-    Danger
+    <span class="text-lg">Do you really mean to delete this org? Type below "{{ orgName }}"</span>
+    <form class="mt-5" action="/api/orgs/delete-org" method="get">
+        <div class="flex flex-col gap-3 justify-start items-start">
+            <InputText name="orgName" placeholder='Here type your org id' required />
+            <Button type="submit" label="Delete" severity="danger" />
+        </div>
+    </form>
 </template>
+
+<script setup lang="ts">
+const { params: { orgName } } = useRoute()
+</script>
---
+++ server/api/orgs/delete-org.get.ts
@@ -0,0 +1,35 @@
+export default defineEventHandler(async (event) => {
+    const { orgName } = await getValidatedQuery(event, z.object({
+        orgName: z.string().trim().min(1),
+    }).parse)
+    const db = event.context.drizzle
+    const userId = event.context.auth.userId
+    const canEdit = await roleEditAllowed(event, orgName)
+    if (!canEdit) {
+        throw createError({
+            message: 'You cannot do this to org',
+            statusCode: 400,
+        })
+    }
+
+    const org = await db.select({
+        id: tables.organizations.id,
+    })
+        .from(tables.organizations)
+        .leftJoin(tables.organizationsPeople, eq(tables.organizationsPeople.organizationId, tables.organizations.id))
+        .where(and(
+            eq(tables.organizations.name, orgName),
+            eq(tables.organizationsPeople.userId, userId),
+        ))
+        .then(takeUniqueOrThrow)
+    await db.batch([
+        db.delete(tables.apps)
+            .where(eq(tables.apps.organizationsId, org.id)),
+        db.delete(tables.organizationsPeople)
+            .where(eq(tables.organizationsPeople.organizationId, org.id)),
+        db.delete(tables.organizations)
+            .where(eq(tables.organizations.id, org.id)),
+    ])
+
+    await sendRedirect(event, '/apps')
+})
