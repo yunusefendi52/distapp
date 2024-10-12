477041ea3ca894acbb5201f496836894cc456aba
Yunus <yunus.rectangle951@silomails.com>
2024-10-12 11:44:03 +0700

    Add favicon

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,9 +1,13 @@
 <template>
-    <div class="flex flex-row gap-4 items-center px-5" style="height: 58px;">
+    <div class="flex flex-row gap-4 items-center px-5" style="height: 62px;">
         <div class="w-8 flex items-center justify-center">
-            <label class="pi pi-box" style="font-size: 1.875rem;" />
+            <AppIcon class="fill-black dark:fill-white" />
         </div>
         <label class="font-semibold text-xl">DistApp</label>
     </div>
     <!-- <AppDivider orientation="horizontal" /> -->
 </template>
+
+<script setup lang="ts">
+import AppIcon from '~/public/appicon.svg'
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -116,6 +116,32 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: 'DistApp',
+      link: [{
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      }, {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      }, {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      }, {
+        rel: 'manifest',
+        href: '/site.webmanifest',
+      }, {
+        rel: 'mask-icon',
+        href: '/safari-pinned-tab.svg',
+        color: '#717171',
+      }],
+      meta: [{
+        name: 'theme-color',
+        content: 'theme-color',
+      }],
     },
   },
   postcss: {
@@ -129,6 +155,7 @@ export default defineNuxtConfig({
     '@hebilicious/vue-query-nuxt',
     '@vueuse/nuxt',
     '@primevue/nuxt-module',
+    'nuxt-svgo-loader'
   ],
 
   primevue: {
