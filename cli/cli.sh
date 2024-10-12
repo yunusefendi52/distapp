e034f4e2658bb264027176cb95c25fed8c4bf724
Yunus <yunus.rectangle951@silomails.com>
2024-10-12 10:11:49 +0700

    Disable cache control curl

--- cli/cli.sh
+++ cli/cli.sh
@@ -2,5 +2,5 @@
 
 scriptUrl="https://github.com/yunusefendi52/distapp/raw/refs/heads/main/cli/cli.mjs"
 scriptPath="/tmp/distapp-cli.mjs"
-curl -L -o $scriptPath "$scriptUrl"
+curl -L -o $scriptPath -H 'Cache-Control: no-cache'  "$scriptUrl"
 node $scriptPath "$@"
