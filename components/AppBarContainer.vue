d1b84a6902697acad237058189e4234cad8b87a7
Yunus <yunus.rectangle951@silomails.com>
2024-11-03 22:00:33 +0700

    fix height

--- components/AppBarContainer.vue
+++ components/AppBarContainer.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="flex flex-col gap-3 sm:flex-row px-6 items-center py-3" style="min-height: 60px;">
+        <div class="flex flex-col gap-3 sm:flex-row px-6 items-center py-3" style="min-height: 62px;">
             <slot></slot>
         </div>
         <AppDivider orientation="horizontal" />
