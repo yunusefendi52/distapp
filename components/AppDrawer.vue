5fd8bba79576dd69f406434e0132b5dc1816ccd5
Yunus <yunus.rectangle951@silomails.com>
2024-11-04 21:18:33 +0700

    Fix bg

--- components/AppDrawer.vue
+++ components/AppDrawer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col h-full dark:bg-[rgb(25,25,25)] bg-[var(--p-surface-50)]">
+    <div class="flex flex-col h-full bg-color">
         <div class="flex-1 overflow-auto">
             <AppHeader />
             <AppMenu />
