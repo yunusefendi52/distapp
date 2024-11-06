ca364e6a1d37ce493135da12e4588f5283a18d5a
Yunus <yunus.rectangle951@silomails.com>
2024-11-06 21:41:39 +0700

    Add validation delete org

--- pages/orgs/[orgName]/settings/danger.vue
+++ pages/orgs/[orgName]/settings/danger.vue
@@ -1,6 +1,6 @@
 <template>
     <span class="text-lg">Do you really mean to delete this org? Type below "{{ orgName }}"</span>
-    <form class="mt-5" action="/api/orgs/delete-org" method="get">
+    <form ref="form" class="mt-5" action="/api/orgs/delete-org" method="get">
         <div class="flex flex-col gap-3 justify-start items-start">
             <InputText name="orgName" placeholder='Here type your org id' required />
             <Button type="submit" label="Delete" severity="danger" />
@@ -10,4 +10,20 @@
 
 <script setup lang="ts">
 const { params: { orgName } } = useRoute()
+
+const form = ref<HTMLFormElement>()
+if (import.meta.client) {
+    onMounted(() => {
+        form.value!.onsubmit = function (ev) {
+            const objectForm = getObjectForm(ev)
+            if (objectForm.orgName != orgName) {
+                alert("Org id is not valid")
+                return false;
+            }
+        }
+    })
+    onUnmounted(() => {
+        form.value!.onsubmit = null;
+    })
+}
 </script>
