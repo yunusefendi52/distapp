f54d82cfed0d082a42564ed966b8b0279016b454
Yunus <yunus.rectangle951@silomails.com>
2025-01-01 14:41:43 +0700

    Fix icon not showing up in safari

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-row gap-4 items-center pl-5" style="height: 62px;">
         <div class="w-8 flex items-center justify-center">
-            <AppIcon class="fill-black dark:fill-white" />
+            <AppIcon class="fill-black dark:fill-white h-8" />
         </div>
         <label class="font-semibold text-xl flex-1">DistApp</label>
         <ThemeSwitcher />
