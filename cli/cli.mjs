83ec72ff42095987b8afd649d18b158ddc5dcc3c
Yunus <yunus.rectangle951@silomails.com>
2025-01-04 17:41:19 +0700

    update cli

--- cli/cli.mjs
+++ cli/cli.mjs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4133bd69db4a6f367633f8223d5444aa249beddac8770fbb3f0f550445bb3003
-size 635569
+oid sha256:ad2af022ba0fcbe7fd58f2068ec21da49d6f94247caae77dc9f4051138557c9a
+size 635863
