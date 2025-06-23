9e8d97b3bd2603b56268c0a4f3d4a7980aeb43c7
Yunus Efendi <19399214+yunusefendi52@users.noreply.github.com>
2025-06-24 06:26:40 +0700

    Add QR link in group to open link (#25)
    * show qr
    
    * update margin
    
    * update ui
    
    * update

---
+++ components/QrCodeView.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import QRCodeStyling from 'qr-code-styling'
+
+const divQr = useTemplateRef('divQr')
+
+const props = defineProps<{
+    code: string,
+}>()
+onMounted(() => {
+    watchEffect(() => {
+        const qrCode = new QRCodeStyling({
+            width: 250,
+            height: 250,
+            type: 'svg',
+            data: props.code,
+            image: "",
+            margin: 0,
+            dotsOptions: {
+                type: 'classy',
+            },
+            backgroundOptions: {
+                color: "#ffffff",
+            },
+            imageOptions: {
+                crossOrigin: "anonymous",
+                // margin: 40,
+            },
+        })
+        divQr.value!.innerHTML = ''
+        qrCode.append(divQr.value!)
+    })
+})
+</script>
+
+<template>
+    <div class="flex items-center justify-center bg-white py-1 px-4">
+        <div ref="divQr"></div>
+    </div>
+</template>
