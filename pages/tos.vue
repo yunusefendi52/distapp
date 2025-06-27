bb1ca7517179b3d32c9957063653888de71a3310
Yunus <yunus.rectangle951@silomails.com>
2025-06-27 10:49:51 +0700

    Update tos

--- pages/tos.vue
+++ pages/tos.vue
@@ -14,12 +14,7 @@
                         to block the account.</li>
                     <li>We are not liable for any damages, including but not limited to, suspension or cessation of
                         DistApp hosting and circumstances caused by user's fault, e.g. user's software.</li>
-                    <li>We offer a paid service. You can sign up for a monthly or yearly
-                        subscription, which will automatically renew on a monthly or yearly basis. If you cancel
-                        your
-                        subscription, you will not receive a refund or credit for any amounts that have already been
-                        paid. We have the right to change our service pricing at any time. Changes to pricing will
-                        not apply until your next renewal.</li>
+                    <li>We offer a paid service. Each subscription includes 5GB of data usage per month. Any usage beyond 5GB will be billed at a rate of $0.02 per additional GB. You can sign up for a monthly subscription, which will automatically renew unless canceled. If you cancel your subscription, you will not receive a refund or credit for any amounts already paid. Your data will be retained for 30 days after cancellation, after which it will be permanently deleted. We reserve the right to change our service pricing at any time. Changes to pricing will not apply until your next renewal.</li>
                     <li>We reserves the right to change these terms.</li>
                 </ol>
             </div>
