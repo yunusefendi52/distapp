ca5d5c39eddfd74e3e7a095926a777f8795155aa
Yunus <19399214+yunusefendi52@users.noreply.github.com>
2025-08-04 00:37:05 +0700

    Fix delete artifact didn't delete apk object

--- server/api/artifacts/delete-artifact.ts
+++ server/api/artifacts/delete-artifact.ts
@@ -1,8 +1,8 @@
 import { S3Fetch } from "~/server/services/s3fetch"
 import { getDetailArtifact } from "./detail-artifact.get"
+import type { EventHandlerRequest, H3Event } from "h3"
 
 export default defineEventHandler(async (event) => {
-    const db = event.context.drizzle
     const { appName, orgName, releaseId } = await getValidatedQuery(event, z.object({
         appName: z.string().trim().min(1).max(128),
         orgName: z.string().trim().min(1).max(128),
@@ -20,22 +20,39 @@ export default defineEventHandler(async (event) => {
         appName,
         releaseId,
     )
-    const { assets } = getStorageKeys(userOrg.org!.id, app.id, detailArtifact.fileObjectKey)
-    const s3 = new S3Fetch()
-    await db.transaction(async t => {
-        await s3.deleteObject(assets)
-        await t.delete(tables.artifactsGroupsManager)
-            .where(and(
-                eq(tables.artifactsGroupsManager.artifactsId, detailArtifact.id),
-            ))
-        await t.delete(tables.artifacts)
-            .where(and(
-                eq(tables.artifacts.id, detailArtifact.id),
-                eq(tables.artifacts.appsId, app.id),
-            ))
-    })
+
+    await deleteArtifact(event, detailArtifact.id, userOrg.org!.id, app.id, detailArtifact.fileObjectKey, detailArtifact.fileObjectApkKey)
 
     return {
         deleted: new Date(),
     }
 })
+
+export async function deleteArtifact(
+    event: H3Event<EventHandlerRequest>,
+    artifactId: string,
+    orgId: string,
+    appId: string,
+    fileObjectKey: string,
+    fileObjectApkKey: string | null,
+) {
+    const { assets } = getStorageKeys(orgId, appId, fileObjectKey)
+    const assetsApkKey = fileObjectApkKey ? getStorageKeys(orgId, appId, fileObjectApkKey).assets : undefined
+    const db = event.context.drizzle
+    const s3 = new S3Fetch()
+    await db.transaction(async t => {
+        await s3.deleteObject(assets)
+        if (assetsApkKey) {
+            await s3.deleteObject(assetsApkKey)
+        }
+        await t.delete(tables.artifactsGroupsManager)
+            .where(and(
+                eq(tables.artifactsGroupsManager.artifactsId, artifactId),
+            ))
+        await t.delete(tables.artifacts)
+            .where(and(
+                eq(tables.artifacts.id, artifactId),
+                eq(tables.artifacts.appsId, appId),
+            ))
+    })
+}
